eccf6e9a622c464f9f8f38cca6e7b30e71e5b6a2
= <=>
2021-02-07 12:17:21 -0300

    Aula 11

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Primeiro from './components/basicos/Primeiro';
 import "./index.css";
 
-
-const tag = <strong>Olá Mundo - React tag </strong>;
 ReactDOM.render(
     <div>
-        {tag}
+        <Primeiro></Primeiro>
+        <Primeiro></Primeiro>
     </div>,
     document.getElementById('root')
 )
