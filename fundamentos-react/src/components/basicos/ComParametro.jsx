7d9a33df49cec7f350dca880717a0bce389f04f6
= <=>
2021-02-07 12:53:00 -0300

    Aula 14

--- fundamentos-react/src/components/basicos/ComParametro.jsx
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 
 export default function ComParametro(props) {
     console.log(props);
+    const status = props.nota >= 7 ? "Aprovado" : "Recuperação";
+    //props.nota = Math.ceil(props.nota); //Vide TIP, dá erro. 
+    const notaInt = Math.ceil(props.nota);
     return (
         <div>
             <h2>Titulo: {props.titulo}</h2>
             <h3>subtitulo: {props.subTitulo}</h3>
-            <p>Nota: { props.nota }</p>
+            <p>Nota: { notaInt }</p>
+            <p>Status: { status } </p>
         </div>
     )
 }
 
 /**
+ * TIP Aula 14: PROPS SÃO SOMENTE LEITURA!
+ * 
  * TIP AULA 13: 
  * Ao passar o parametro nota com {}, ele receberá o tipo numerico. 
  * Ao passar o parametro nota com "" ele receberá o tipo string
