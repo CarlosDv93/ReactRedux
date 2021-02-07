aba0a43557a9f50b496e6b64f31d7092e9b3c9c2
= <=>
2021-02-07 12:07:13 -0300

    Aula 10 - css

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
 
 
-
-const tag = <strong>Olá Mundo - React tag</strong>;
+const tag = <strong>Olá Mundo - React tag </strong>;
 ReactDOM.render(
     <div>
         {tag}
