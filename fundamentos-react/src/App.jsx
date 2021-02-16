6a0b9cb0ed00baa08ed997369ec81252331d3ec0
= <=>
2021-02-16 13:09:21 -0300

    Aula 16 parte 2

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -5,12 +5,28 @@ import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 
 
-export default function App(props) {
+export default _ =>
+    <div id="app">
+        <h1>React - Fundamentos React _</h1>
+        <Fragmento />
+
+        <ComParametro titulo="Situação do Aluno"
+            subTitulo="Pedro"
+            nota={9.3} />
+
+        <Primeiro></Primeiro>
+    </div>
+
+/**
+ *
+ * Alternativa 01:
+ ``` js
+ export default function (props) {
     return (
         <div id="app">
             <h1>React - Fundamentos React</h1>
             <Fragmento />
-            
+
             <ComParametro titulo="Situação do Aluno"
                 subTitulo="Pedro"
                 nota={9.3} />
@@ -19,3 +35,76 @@ export default function App(props) {
         </div>
     )
 }
+```
+ * Alternativa 02:
+ ``` js
+export default (props) => {
+    return (
+        <div id="app">
+            <h1>React - Fundamentos React (props)</h1>
+            <Fragmento />
+
+            <ComParametro titulo="Situação do Aluno"
+                subTitulo="Pedro"
+                nota={9.3} />
+
+            <Primeiro></Primeiro>
+        </div>
+    )
+}
+```
+ *
+ * Alternativa 03:
+ ``` js
+export default props => {
+    return (
+        <div id="app">
+            <h1>React - Fundamentos React props</h1>
+            <Fragmento />
+
+            <ComParametro titulo="Situação do Aluno"
+                subTitulo="Pedro"
+                nota={9.3} />
+
+            <Primeiro></Primeiro>
+        </div>
+    )
+}
+```
+ *
+ * Alternativa 04:
+ ``` js
+export default props => {
+    return (
+        <div id="app">
+            <h1>React - Fundamentos React props</h1>
+            <Fragmento />
+
+            <ComParametro titulo="Situação do Aluno"
+                subTitulo="Pedro"
+                nota={9.3} />
+
+            <Primeiro></Primeiro>
+        </div>
+    )
+}
+```
+ *
+ * Alternativa 05: (Mais reduzida, pois não usamos o parametro passado)
+ ``` js
+export default _ => {
+    return (
+        <div id="app">
+            <h1>React - Fundamentos React _</h1>
+            <Fragmento />
+
+            <ComParametro titulo="Situação do Aluno"
+                subTitulo="Pedro"
+                nota={9.3} />
+
+            <Primeiro></Primeiro>
+        </div>
+    )
+}
+```
+ */
